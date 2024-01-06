750ec8a78d7df7f3738c03a7b8e03be4b1426358
Nikos <nik.avg@hotmail.com>
2024-01-06 12:07:24 +0200

    Added update db functionality

--- update_data.py
+++ update_data.py
@@ -1,5 +1,5 @@
 from price_data_todb_final import update_kraken_data_in_sqlite
-from text_data_to_db_final import update_reddit_data_in_sqlite
+from text_data_todb_final import update_reddit_data_in_sqlite
 
 def update_databases():
     symbols = ['BTC/EUR', 'ETH/EUR', 'XMR/EUR', 'ADA/EUR', 'DOT/EUR']
